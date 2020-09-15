9c1ab89c658d0077c07f038bcbf22b9b224d99ff
profaddy <adnansaify11@gmail.com>
2020-09-15 10:24:28 +0530

    removed unwanted code and necessary updates pushed

--- pages/_app.js
+++ pages/_app.js
@@ -2,29 +2,24 @@ import App from 'next/app';
 import Head from 'next/head';
 import { CookiesProvider } from "react-cookie";
 import { AppProvider } from '@shopify/polaris';
-import { Provider } from '@shopify/app-bridge-react';
-import Cookies from "js-cookie";
 import '@shopify/polaris/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
 
     return (
       <React.Fragment>
         <Head>
-          <title>Sample App</title>
+          <title>My First Shopify app</title>
           <meta charSet="utf-8" />
         </Head>
-        <Provider config={config} >
-          <AppProvider i18n={translations}>
+          <AppProvider i18n={translations} >
               <CookiesProvider>
-              <Component {...pageProps} config={config}/>
+              <Component {...pageProps}/>
               </CookiesProvider>
           </AppProvider>
-        </Provider>
       </React.Fragment>
     );
   }
--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
 const next = require('next');
-
+const fs = require('fs');
+const https = require('https');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -20,35 +21,68 @@ const router = new Router();
 const {
   SHOPIFY_API_SECRET_KEY,
   SHOPIFY_API_KEY,
+  APP_NAME,
 } = process.env;
 app.prepare().then(() => {
   const server = new Koa(app);
+
+  router.get('/GetShopifyCustomerdata', async (ctx) => {
+      try {
+          ctx.response.status = 200;
+      } catch (err) {
+          console.log(err)
+      }
+  });
+  router.get('/EraseShopifyCustomerdata', async (ctx) => {
+      try {
+          ctx.response.status = 200;
+      } catch (err) {
+          console.log(err)
+      }
+  });
+  router.get('/EraseShopData', async (ctx) => {
+      try {
+          ctx.response.status = 200;
+      } catch (err) {
+          console.log(err)
+      }
+  });
+  server.use(router.routes());
+  server.use(
+      bodyParser({
+          detectJSON: function (ctx) {
+              return /\.json$/i.test(ctx.path);
+          },
+      })
+  );
+  server.use(session({ sameSite: 'none', secure: true }, server));
   server.use(session({ sameSite: 'none', secure: true }, server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
-
-  server.use(
-    createShopifyAuth({
-      apiKey: SHOPIFY_API_KEY,
-      secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ['read_products', 'write_products'],
-      async afterAuth(ctx) {
-        const { shop, accessToken } = ctx.session;
-        ctx.cookies.set("shopOrigin", shop, {
-          httpOnly: false,
-          secure: true,
-          sameSite: 'none'
-        });
-        ctx.cookies.set("accessToken", accessToken, {
-          httpOnly: false,
-          secure: true,
-          sameSite: 'none'
-        });
-        console.log("afterAuth");
-        ctx.redirect('/')
-      }
-    })
-  );
-  server.use(verifyRequest());
+  if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "development") {
+    server.use(
+      createShopifyAuth({
+        apiKey: SHOPIFY_API_KEY,
+        secret: SHOPIFY_API_SECRET_KEY,
+        scopes: ['read_products', 'write_products'],
+        async afterAuth(ctx) {
+          const { shop, accessToken } = ctx.session;
+          ctx.cookies.set("shopOrigin", shop, {
+            httpOnly: false,
+            secure: true,
+            sameSite: 'none'
+          });
+          ctx.cookies.set("accessToken", accessToken, {
+            httpOnly: false,
+            secure: true,
+            sameSite: 'none'
+          });
+          console.log("afterAuth");
+          ctx.redirect(`https://${shop}/admin/apps/${APP_NAME}`);
+        }
+      })
+    );
+    server.use(verifyRequest());
+  }
   server.use(bodyParser());
   server.use(router.routes());
   server.use(async (ctx) => {
@@ -58,8 +92,39 @@ app.prepare().then(() => {
     return
   });
   server.use(cors())
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+  const serverCallback = server.callback();
+  if (process.env.NODE_ENV === 'production') {
+      var privateKey = fs.readFileSync(
+          '/etc/letsencrypt/live/productedit.vowelweb.com/privkey.pem',
+          'utf8'
+      );
+      var certificate = fs.readFileSync(
+          '/etc/letsencrypt/live/productedit.vowelweb.com/fullchain.pem',
+          'utf8'
+      );
+      const config = {
+          https: {
+              options: {
+                  key: privateKey,
+                  cert: certificate,
+              },
+          },
+      };
+      const httpsServer = https.createServer(
+          config.https.options,
+          serverCallback
+      );
+      httpsServer.listen(port, function (err) {
+          console.log(`> Ready on production http://localhost:${port}`);
+          if (!!err) {
+              console.error('HTTPS server FAIL: ', err, err && err.stack);
+          }
+      });
+  } else {
+      server.listen(port, () => {
+          console.log(`> Ready on development http://localhost:${port}`);
+      });
+  }
 });
+
 
